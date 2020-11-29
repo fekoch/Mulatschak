83b07a487651fbd83057c4677bc55cc337bfecf7
Felix Koch <fkoch@student.tgm.ac.at>
2020-11-29 19:14:07 +0100

    add a first controller stub

---
+++ src/gui/controller.js
@@ -0,0 +1,85 @@
+/**
+ * A Controller for a Game
+ * @author Felix Koch
+ * @version 2020-11-29
+ */
+class Controller {
+    view;
+    model;
+
+    /**
+     * Creates a new Controller
+     * @param view {PlayGame}
+     */
+    constructor(view) {
+        this.view = view;
+    }
+
+    /*
+     * For everybody:
+     * Game { * Plays { 5 Rounds { 4 Runs
+     * '{' = consists of
+     */
+
+    /**
+     * Plays a Game which consists of an undefined number of Plays
+     */
+    playGame() {
+        // TODO implement
+        this.model = new Model();
+
+        this.startPlay();
+    }
+
+    /**
+     * Starts a Play of 5 Rounds
+     * (because everyone has 5 Cards)
+     */
+    startPlay() {
+        // TODO implement
+
+        this.newRound();
+    }
+
+    /**
+     * Starts a Round of 4 Runs
+     * (one run for each player)
+     */
+    newRound() {
+        // TODO implement ?
+        this.model.handOut();
+
+        this.newRun();
+    }
+
+    /**
+     * Starts a run for the currently active Player
+     *  or ends the Round if all players played
+     */
+    newRun() {
+        let currPlayer = this.model.getSpieleranderReihe();
+        if (currPlayer === -1) {
+            // TODO end of round
+            console.log("end of round");
+        }
+        else if (currPlayer === this.model.getPlayer()) {
+            // TODO player input
+            this.view.showHand(currPlayer.getHand());
+            this.view.displayDropzones(this.playCard);
+        }
+        else {
+            // TODO com play
+            console.log("COM ist an der Reihe");
+        }
+
+    }
+
+    /**
+     * Plays a Card for the Player
+     * @param card the card that the player plays
+     */
+    playCard(card) {
+        console.log("Played CARD:");
+        console.log(card);
+    }
+}
