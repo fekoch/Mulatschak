9f62d85b2d98f22150b0c9c6c3a69afa1c40331c
Felix Koch <fkoch@student.tgm.ac.at>
2020-12-11 10:04:31 +0100

    finnish displayTrickPicker()

--- src/gui/index.js
+++ src/gui/index.js
@@ -568,24 +568,55 @@ class PlayGame extends Phaser.Scene {
      *  ...
      *  1 = [yield,5]
      *  0 = [yield]
+     *  @param listener {Controller} Objekt mit der Callback-Methode `tricksChosen(number)`
      */
-    displayTrickPicker(choices = 5) {
+    displayTrickPicker(choices = 5,listener=this.controller) {
         let gr = this.add.graphics();
         let options = [];
         let texts = [];
         const sh = game.config.height;
-        const yoff = sh/2-sh/10;
+        const yoff = sh / 2 - sh / 10;
         const sw = game.config.width;
         const margin = 20;
-        const offset = (sw - 2*margin)/5;
+        const offset = (sw - 2 * margin) / 5;
 
-        for (let i = 1; i <= choices; i++) {
-            if (i < (5-choices) ) {
+        for (let i = 1; i <= 5; i++) {
+            let tcolor;
+            let r;
+            if (i < (6 - choices)) {
+                r = this.add.rectangle(margin + offset * (i - 1) + offset / 2, yoff, offset * 0.8, offset * 0.8, 0x838383).setOrigin(0.5, 0.5).setStrokeStyle(2, 0x000000);
+                tcolor = 0xBBBBBB;
+            } else {
+                r = this.add.rectangle(margin + offset * (i - 1) + offset / 2, yoff, offset * 0.8, offset * 0.8, 0xffffff).setOrigin(0.5, 0.5).setStrokeStyle(2, 0x000000);
+                r.setInteractive();
+                r.on('pointerup', function (pointer, localx, localy, event) {
+                    for (let j = 0; j < options.length; j++) {
+                        options[j].destroy();
+                        texts[j].destroy();
+                    }
+                    //console.log("clicked: i=" + i)
+                    listener.tricksChosen(i);
+                }, this);
+                tcolor = 0x000000;
             }
-            else{
-                let r = this.add.rectangle(margin+offset*(i-1),yoff,offset,offset,0xffffff).setStroke(2,0x000000);
-            }
+            let bitmaptext = this.add.bitmapText(margin + offset * (i - 1) + offset / 2, yoff, "gothic", "" + i).setOrigin(0.5, 0.5).setTintFill(tcolor);
+
+            options.push(r);
+            texts.push(bitmaptext);
         }
+
+        let r = this.add.rectangle(margin + offset * 0.5, yoff + offset / 2 * 1.2, 4 * offset, offset * 0.6, 0xffffff).setOrigin(0, 0).setStrokeStyle(2, 0x000000).setInteractive();
+        r.on('pointerup', function (pointer, localx, localy, event) {
+            for (let j = 0; j < options.length; j++) {
+                options[j].destroy();
+                texts[j].destroy();
+            }
+            //console.log("clicked: passen")
+            listener.tricksChosen(0);
+        }, this);
+        let t = this.add.bitmapText(sw / 2, yoff + offset / 2 * 1.2 + offset * 0.3, "gothic", "Passen").setOrigin(0.5, 0.5).setTintFill(0x000000);
+        options.push(r);
+        texts.push(t);
     }
 }
 
--- src/gui/test.js
+++ src/gui/test.js
@@ -10,6 +10,7 @@ var config = {
     },
     parent: 'phaser-example',
     scene: {
+        preload: preload,
         create: create
     }
 };
@@ -17,10 +18,12 @@ var config = {
 var game = new Phaser.Game(config);
 
 var graphics;
-
+function preload() {
+    this.load.bitmapFont('gothic','assets/fonts/gothic.png','assets/fonts/gothic.xml');
+}
 function create ()
 {
-    const choices = 5;
+    const choices = 0;
     
     let gr = this.add.graphics();
     let options = [];
@@ -32,17 +35,39 @@ function create ()
     const offset = (sw - 2*margin)/5;
 
     for (let i = 1; i <= 5; i++) {
-        // todo text
+        let tcolor;
+        let r;
         if (i < (6-choices) ) {
-            let r = this.add.rectangle(margin+offset*(i-1)+offset/2,yoff,offset*0.8,offset*0.8,0x838383).setOrigin(0.5,0.5).setStrokeStyle(2,0x000000);
+            r = this.add.rectangle(margin+offset*(i-1)+offset/2,yoff,offset*0.8,offset*0.8,0x838383).setOrigin(0.5,0.5).setStrokeStyle(2,0x000000);
+            tcolor =0xBBBBBB;
         }
         else{
-            let r = this.add.rectangle(margin+offset*(i-1)+offset/2,yoff,offset*0.8,offset*0.8,0xffffff).setOrigin(0.5,0.5).setStrokeStyle(2,0x000000);
+            r = this.add.rectangle(margin+offset*(i-1)+offset/2,yoff,offset*0.8,offset*0.8,0xffffff).setOrigin(0.5,0.5).setStrokeStyle(2,0x000000);
             r.setInteractive();
             r.on('pointerup',function (pointer,localx,localy,event) {
-                // todo
+                for (let j = 0; j < options.length ; j++) {
+                    options[j].destroy();
+                    texts[j].destroy();
+                }
+                console.log("clicked: i="+i)
             },this);
-            // todo hover
+            tcolor =0x000000;
         }
+        let bitmaptext = this.add.bitmapText(margin+offset*(i-1)+offset/2,yoff,"gothic",""+i).setOrigin(0.5,0.5).setTintFill(tcolor);
+
+        options.push(r);
+        texts.push(bitmaptext);
     }
+
+    let r = this.add.rectangle(margin+offset*0.5,yoff+offset/2*1.2,4*offset,offset*0.6,0xffffff).setOrigin(0,0).setStrokeStyle(2,0x000000).setInteractive();
+    r.on('pointerup',function (pointer,localx,localy,event) {
+        for (let j = 0; j < options.length ; j++) {
+            options[j].destroy();
+            texts[j].destroy();
+        }
+        console.log("clicked: passen")
+    },this);
+    let t = this.add.bitmapText(sw/2,yoff+offset/2*1.2+offset*0.3,"gothic","Passen").setOrigin(0.5,0.5).setTintFill(0x000000);
+    options.push(r);
+    texts.push(t);
 }
